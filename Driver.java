9b20ec1a7f3fedf73d7078a350d76725fab52464
anafdz <afernan6@wellesley.edu>
2017-05-08 13:42:00 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -9,18 +9,22 @@ import java.awt.event.*;
 
 public class Driver extends JPanel {
   
+  //instance variables
   private static MapsBuilder graph;
   private static AdjListsGraph<Room> roomGraph;
   
   public static void main (String[] args) {   
-    
+    //creates new instances
     graph = new MapsBuilder();
     roomGraph = graph.build("allrooms.tgf");
     
+    //creates jframe that our panes will be added
     JFrame frame = new JFrame ("Layout Manager");  
     frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);  
     
-    //Add in the start of the program
+    //adds tabs to the pane
+    //first tab is the about/intro page
+    //second tab is where the magic happens
     JTabbedPane tp = new JTabbedPane();        
     tp.addTab ("SciMaps", new About());
     tp.addTab ("Get Directions", new HomePage(roomGraph));
