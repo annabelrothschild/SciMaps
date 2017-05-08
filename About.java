bbfc36320ec907daa9680b999da6358dd98b256c
anafdz <afernan6@wellesley.edu>
2017-05-08 14:09:19 -0400

    Update About.java

--- About.java
+++ About.java
@@ -7,21 +7,29 @@
 import java.awt.*; 
 import javax.swing.*; 
 
+//introduction panel
 public class About extends JPanel  {   
   private ImageIcon memetastic;
   public About() { 
     setBackground (Color.cyan);  
     setLayout(new FlowLayout());
+    
+    //displays intro message
     JLabel l1 = new JLabel ("<html> Got lost in the maze that is the Science Center? Worry no longer!" +  
-                              "<br><br>SciMaps, designed by Ana Fernandez, Annabel Rothschild, and Meha Ahluwalia will get you to your destination." + 
+                              "<br><br>SciMaps, designed by Ana Fernandez, Annabel Rothschild, and Meha Ahluwalia will get you                                to your destination." + 
                               "<br>" + 
                               "<br>Select the 'Get Directions' tab to start your navigation!</html>");    
     add (l1);
+    
+    //displays a picture of a woman looking confused surrounded by math equations
+    //this picture depicts how one feels trying to get around the science center
     JLabel l2 = new JLabel();
     memetastic = createImageIcon("mathlady.png", "the science center tho??");
     l2.setIcon(memetastic);
     add (l2);
+    
   } 
+    //helper method to display picture
     private static ImageIcon createImageIcon(String path, String description) {
     java.net.URL imgURL = AdjListsGraph.class.getResource(path);
     if (imgURL != null) {
